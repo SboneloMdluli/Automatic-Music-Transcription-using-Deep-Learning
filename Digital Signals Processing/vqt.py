309fcd96165ee80a34f791a672f7b777f066a067
Matthews Malatji <1387556@students.wits.ac.za>
2020-10-01 01:45:34 +0200

    Vqt,CQT and mel scaled Spectrograms added to results

--- Digital Signals Processing/vqt.py
+++ Digital Signals Processing/vqt.py
@@ -7,6 +7,30 @@ import librosa
 import librosa.display
 import os
 from glob import glob
+import matplotlib.pyplot as plt
+# For plotting headlessly
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+# Define Variable Q-Transform Parameters for Audio Signals Processing
+fs = 16000  # Sampling frequency
+hop_length = 512  # number of samples between successive VQT columns
+fmin = None  # Minimum frequency. Defaults to C1 ~= 32.70 Hz
+n_bins = 84  # Number of frequency bins
+# Bandwidth offset for determining filter lengths (If Gamma=0 then => CQT computation)
+gamma = 20
+bins_per_octave = 12  # Number of frequency bins per octave
+# Tuning offset in fractions of a bin(None, tuning will be automatically estimated from the signal)
+tuning = 0.0
+# Filter scale factor. Small values (<1) use shorter windows for improved time resolution.
+filter_scale = 1
+norm = 1  # Type of norm to use for basis function normalization
+sparsity = 0.01  # Sparsify the VQT basis by discarding up to sparsity fraction of the energy
+window = 'hann'  # Using Hann Window
+scale = True  # Scale the VQT response by square-root the length of each channel’s filter
+pad_mode = 'reflect'  # Padding mode for centered frame analysis
+res_type = None  # The resampling mode for recursive downsampling
+dtype = None  # The dtype of the output array. By default, this is inferred to match the numerical precision of the input signal
+n_mels = 128
 
 def scale_minmax(X, Xmin=0.0, Xmax=1.0):
     X_std = (X - X.min()) / (X.max() - X.min())
@@ -14,49 +38,33 @@ def scale_minmax(X, Xmin=0.0, Xmax=1.0):
     return X_scaled
 
 def AMT(filename_):
-    # Define Variable Q-Transform Parameters for Audio Signals Processing
-    fs = 16000  # Sampling frequency
-    hop_length = 512  # number of samples between successive VQT columns
-    fmin = None  # Minimum frequency. Defaults to C1 ~= 32.70 Hz
-    n_bins = 84  # Number of frequency bins
-    # Bandwidth offset for determining filter lengths (If Gamma=0 then => CQT computation)
-    gamma = 20
-    bins_per_octave = 12  # Number of frequency bins per octave
-    # Tuning offset in fractions of a bin(None, tuning will be automatically estimated from the signal)
-    tuning = 0.0
-    # Filter scale factor. Small values (<1) use shorter windows for improved time resolution.
-    filter_scale = 1
-    norm = 1  # Type of norm to use for basis function normalization
-    sparsity = 0.01  # Sparsify the VQT basis by discarding up to sparsity fraction of the energy
-    window = 'hann'  # Using Hann Window
-    scale = True  # Scale the VQT response by square-root the length of each channel’s filter
-    pad_mode = 'reflect'  # Padding mode for centered frame analysis
-    res_type = None  # The resampling mode for recursive downsampling
-    dtype = None  # The dtype of the output array. By default, this is inferred to match the numerical precision of the input signal
-    n_mels = 128
-
     # Audio Processing
     # Loading the Audios
     # Path Configuration
     #path = os.getcwd() + '/' + filename_
-    #filename = "{}".format(filename_)
-    #x, fs = librosa.load(filename, sr=None, mono=True, duration=12)
-    
-    path = os.getcwd()
-    #audio_files = glob(path + '/*.wav')     # Collect all wav format files
-    audio_files = glob(path + '/*.wav')     # Collect all 
+    filename = "{}".format(filename_)
+    x, fs = librosa.load(filename, sr=None, mono=True, duration=12)
+   
+    # VQT Computation
+    V = librosa.vqt(x, sr=fs, hop_length=hop_length, fmin=fmin, n_bins=n_bins, gamma=20, bins_per_octave=bins_per_octave, tuning=tuning,
+                        filter_scale=filter_scale, norm=norm, sparsity=0.01, window='hann', scale=scale, pad_mode=pad_mode, res_type=res_type, dtype=dtype)
 
-    for filename in range(0,len(audio_files),1):
-        filename = "{}".format(audio_files[filename])
-        x, fs = librosa.load(audio_files[filename], sr=None, mono=True, duration=12)
-        # VQT Computation
-        V = librosa.vqt(x, sr=fs, hop_length=hop_length, fmin=fmin, n_bins=n_bins, gamma=20, bins_per_octave=bins_per_octave, tuning=tuning,
-                            filter_scale=filter_scale, norm=norm, sparsity=0.01, window='hann', scale=scale, pad_mode=pad_mode, res_type=res_type, dtype=dtype)
+    V_mel = np.abs(V)
+    # Save The spectrogram
+    '''
+    fig = plt.Figure()
+    canvas = FigureCanvas(fig)
+    ax = fig.add_subplot(111)
+    #ax = set_title(filename_+' VQT')
+    p = librosa.display.specshow(librosa.amplitude_to_db(V_mel, ref=np.max), ax=ax, y_axis='log', x_axis='time')
+    out = "{}.png".format(filename_)
+    fig.savefig(out)
+    '''
+    # Conversion into the Mel-Scale to display and save Mel-spectrogram  for prediction  
+    melspec(V_mel,filename)
 
-        V_mel = np.abs(V)
     
-# Conversion into the Mel-Scale to display and save Mel-spectrogram
-def melspec(V_mel):
+def melspec(V_mel,filename):
     # Mapping Magnitude spectrogram to the Mel Scale
     mels = librosa.feature.melspectrogram(
     S=V_mel, sr=fs, n_mels=n_mels, n_fft=hop_length*2, hop_length=hop_length)
